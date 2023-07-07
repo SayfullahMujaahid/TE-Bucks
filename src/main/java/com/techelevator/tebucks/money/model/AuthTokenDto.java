e26452a711fadcf6bb3469b3389a570031c4e776
Anna Lee <annalee5492@gmail.com>
2023-07-07 15:25:06 -0400

    everything works hopefully

---
+++ src/main/java/com/techelevator/tebucks/money/model/AuthTokenDto.java
@@ -0,0 +1,13 @@
+package com.techelevator.tebucks.money.model;
+
+public class AuthTokenDto {
+    String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
